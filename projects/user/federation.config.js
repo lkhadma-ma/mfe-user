cb561eb462007cf360444775fbb7a3d44705fc2e
OUSSAMA YAAGOUB <oyaagoub@externas.sescam.jccm.es>
2025-08-29 18:37:34 +0200

    update 1 file and create 1 file

--- projects/user/federation.config.js
+++ projects/user/federation.config.js
@@ -16,17 +16,10 @@ module.exports = withNativeFederation({
     'rxjs/ajax',
     'rxjs/fetch',
     'rxjs/testing',
-    'rxjs/webSocket',
-    // Add further packages you don't need at runtime
+    'rxjs/webSocket'
   ],
 
-  // Please read our FAQ about sharing libs:
-  // https://shorturl.at/jmzH0
-
   features: {
-    // New feature for more performance and avoiding
-    // issues with node libs. Comment this out to
-    // get the traditional behavior:
     ignoreUnusedDeps: true
   }
   
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./projects/user/src/**/*.{html,ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
