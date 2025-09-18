ddcc7ad8f8a99829461f2eb6e61f3c12df3c3d50
OUSSAMA YAAGOUB <oyaagoub@externas.sescam.jccm.es>
2025-09-18 20:45:01 +0200

    update user-shell.component.ts

--- projects/user/src/app/domains/user/feature/user-shell.component.ts
+++ projects/user/src/app/domains/user/feature/user-shell.component.ts
@@ -27,6 +27,7 @@ export class UsershellComponent {
         if (user) {
           this.name = user.displayName;
           this.photoURL = user.photoURL;
+          console.log("name", this.name, "photoURL", this.photoURL);
         }
       });
     });
